ec54e6f382b840c3e8f702502fbe27744a9725ac
kaleong kong <kaleongkong@yahoo.com>
2017-08-27 22:51:38 -0700

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from 'react-bootstrap';
+export default class App extends React.Component{
+  render(){
+    return (
+      <Button bsStyle='primary'>Default</Button>
+    );  
+  }
+}
